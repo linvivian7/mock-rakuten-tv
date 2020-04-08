518f2d4682883ec214bde133582a4428fe19c7fd
Vivian Lin <linvivian7@gmail.com>
2020-04-09 03:35:36 +0900

    Add movies and tv shows to data dump

--- scripts/data-dump.js
+++ scripts/data-dump.js
@@ -1,23 +1,47 @@
 const axios = require('axios');
 const camelCaseKeys = require('camelcase-keys');
 const fs = require('fs');
-const Path = require('path');
 
 const FILE_DIR = './static/data';
-
-const TOP_URL =
-  'https://gizmo.rakuten.tv/v3/gardens/default?classification_id=5&device_identifier=web&locale=es&market_code=es&user_status=visitor';
+const PREFIX = 'https://gizmo.rakuten.tv/v3';
+const SUFFIX =
+  '?classification_id=5&device_identifier=web&locale=es&market_code=es&user_status=visitor';
+const TOP_URL = `${PREFIX}/gardens/default${SUFFIX}`;
 
 const getImageKitUrl = (url) =>
   url && `https://ik.imagekit.io/hqe2xslcea/${url.split('system/')[1]}`;
 
+const movieIds = [];
+const tvIds = [];
+
+const writeFile = (filename, data) => {
+  fs.writeFile(`${FILE_DIR}/${filename}.json`, JSON.stringify(data), (err) => {
+    if (err) {
+      throw new Error(err);
+    }
+
+    console.log(`${filename} JSON file saved.`);
+  });
+};
+
+const helper = ({ results, fileName }) => {
+  const raw = {};
+
+  results.forEach((result) => {
+    raw[result.key] = result.value;
+  });
+
+  writeFile(fileName, raw);
+};
+
+const formatResult = (id, result) => ({ key: id, value: result.data.data });
+
 const defaultNormalize = ({ data: { lists, genres } }) => {
-  const allMovies = {};
   const movieLists = lists.map(({ id, name, contents }) => ({
     id,
     name,
     movies: contents.data.map((movie) => {
-      const { id, images } = movie;
+      const { id, images, type } = movie;
       const cover = `${getImageKitUrl(images.artwork)}?tr=w-224,h-auto`;
       const coverLQ = `${cover},bl-30,q-50`;
       const snapshot = `${getImageKitUrl(images.snapshot)}`;
@@ -36,7 +60,11 @@ const defaultNormalize = ({ data: { lists, genres } }) => {
         { deep: true }
       );
 
-      allMovies[id] = info;
+      if (type === 'movies') {
+        movieIds.push(id);
+      } else if (type === 'tv_shows') {
+        tvIds.push(id);
+      }
 
       return info;
     }),
@@ -45,7 +73,6 @@ const defaultNormalize = ({ data: { lists, genres } }) => {
   const movieGenres = genres.data.map((genre) => camelCaseKeys(genre));
 
   return {
-    movies: allMovies,
     movieLists,
     movieGenres,
   };
@@ -55,20 +82,6 @@ axios
   .get(TOP_URL)
   .then((result) => defaultNormalize(result.data))
   .then((data) => {
-    const writeFile = (filename, data) => {
-      fs.writeFile(
-        `${FILE_DIR}/${filename}.json`,
-        JSON.stringify(data),
-        (err) => {
-          if (err) {
-            throw new Error(err);
-          }
-
-          console.log(`${filename} JSON file saved.`);
-        }
-      );
-    };
-
     try {
       fs.mkdirSync(FILE_DIR);
       console.log(`${FILE_DIR} folder created.`);
@@ -76,7 +89,22 @@ axios
       console.log(`${FILE_DIR} folder existed.`);
     }
 
-    writeFile('movies', data.movies);
+    Promise.all(
+      movieIds.map((id) =>
+        axios
+          .get(`${PREFIX}/movies/${id}${SUFFIX}`)
+          .then((result) => formatResult(id, result))
+      )
+    ).then((results) => helper({ results, fileName: 'movies' }));
+
+    Promise.all(
+      tvIds.map((id) =>
+        axios
+          .get(`${PREFIX}/tv_shows/${id}${SUFFIX}`)
+          .then((result) => formatResult(id, result))
+      )
+    ).then((results) => helper({ results, fileName: 'shows' }));
+
     writeFile('lists', data.movieLists);
     writeFile('genres', data.movieGenres);
   });
