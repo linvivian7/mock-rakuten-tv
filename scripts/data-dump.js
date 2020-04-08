a5d1da9f9ec3b2603a106c704e77b603a3c1f56f
Vivian Lin <linvivian7@gmail.com>
2020-04-09 01:28:45 +0900

    Add movie page [WIP]

--- scripts/data-dump.js
+++ scripts/data-dump.js
@@ -8,46 +8,44 @@ const FILE_DIR = './static/data';
 const TOP_URL =
   'https://gizmo.rakuten.tv/v3/gardens/default?classification_id=5&device_identifier=web&locale=es&market_code=es&user_status=visitor';
 
+const getImageKitUrl = (url) =>
+  url && `https://ik.imagekit.io/hqe2xslcea/${url.split('system/')[1]}`;
+
 const defaultNormalize = ({ data: { lists, genres } }) => {
+  const allMovies = {};
   const movieLists = lists.map(({ id, name, contents }) => ({
     id,
     name,
     movies: contents.data.map((movie) => {
-      const artwork =
-        movie.images.artwork &&
-        `https://ik.imagekit.io/hqe2xslcea/${
-          movie.images.artwork.split('system/')[1]
-        }?tr=w-224,h-auto`;
-      const lofi =
-        movie.images.artwork &&
-        `https://ik.imagekit.io/hqe2xslcea/${
-          movie.images.artwork.split('system/')[1]
-        }?tr=w-224,h-auto,bl-30,q-50`;
-      const snapshot =
-        movie.images.snapshot &&
-        `https://ik.imagekit.io/hqe2xslcea/${
-          movie.images.snapshot.split('system/')[1]
-        }`;
-
-      return camelCaseKeys(
+      const { id, images } = movie;
+      const cover = `${getImageKitUrl(images.artwork)}?tr=w-224,h-auto`;
+      const coverLQ = `${cover},bl-30,q-50`;
+      const snapshot = `${getImageKitUrl(images.snapshot)}`;
+      const snapshotLQ = `${getImageKitUrl(images.snapshot)}?tr=bl-30,q-50`;
+      const info = camelCaseKeys(
         {
           ...movie,
           images: {
             ...movie.images,
-            originalArtwork: movie.images.artwork,
-            cover: artwork,
-            coverBlur: lofi,
+            cover,
+            coverBlur: coverLQ,
             snapshot,
+            snapshotBlur: snapshotLQ,
           },
         },
         { deep: true }
       );
+
+      allMovies[id] = info;
+
+      return info;
     }),
   }));
 
   const movieGenres = genres.data.map((genre) => camelCaseKeys(genre));
 
   return {
+    movies: allMovies,
     movieLists,
     movieGenres,
   };
@@ -78,6 +76,7 @@ axios
       console.log(`${FILE_DIR} folder existed.`);
     }
 
+    writeFile('movies', data.movies);
     writeFile('lists', data.movieLists);
     writeFile('genres', data.movieGenres);
   });
--- src/page/Movie/index.js
+++ src/page/Movie/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
 // components
@@ -6,19 +6,54 @@ import TopBanner from '../../component/TopBanner';
 import List from '../../component/List';
 
 // data
-import movieLists from '../../../static/data/lists.json';
+import movies from '../../../static/data/movies.json';
+import sample from '../../../sample-movie.json';
 
 import styles from './Movie.module';
 
 const Movie = ({ match, history }) => {
   const { id } = match.params;
-  console.log('id', id);
+  let movie = movies[id];
+  const hasMovie = !!movie;
+  const [src, setSrc] = useState(hasMovie && movie.images.snapshot);
+
+  if (hasMovie) {
+    useEffect(() => {
+      setTimeout(() => {
+        setSrc(movie.images.snapshot);
+      }, 800);
+    }, []);
+  } else {
+    console.log(sample);
+    movie = sample.data;
+  }
 
   return (
-    <div className={styles.body}>
-      <TopBanner />
-      <div></div>
-    </div>
+    movie && (
+      <div className={styles.body}>
+        <TopBanner />
+        <div
+          style={{ width: '100%', backgroundColor: 'white', height: 'auto' }}
+        >
+          <img className={styles.snapshot} src={src} alt="snapshot" />
+        </div>
+        {/* info */}
+        <div className={styles.info}>
+          <div className={styles.general}>
+            <div>
+              <span>User</span>
+              <span>minutes</span>
+              <span>year</span>
+              <span>country</span>
+              <span>original title</span>
+            </div>
+            <div>label</div>
+            <p>{movie.classification.description}</p>
+          </div>
+          <aside className={styles.social}>aside</aside>
+        </div>
+      </div>
+    )
   );
 };
 
