c596d13d1e75df51d2c73a25004726a48772ff6b
Vivian Lin <linvivian7@gmail.com>
2020-04-11 16:38:28 +0900

    add carousel to Home page

---
+++ src/component/Carousel/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Slider from 'react-slick';
+
+import styles from './Carousel.module';
+import carousel1 from '../../images/carousel-1.jpeg';
+import carousel2 from '../../images/carousel-2.jpeg';
+import carousel3 from '../../images/carousel-3.jpeg';
+import carousel4 from '../../images/carousel-4.jpeg';
+import carousel5 from '../../images/carousel-5.jpeg';
+
+const images = [carousel1, carousel2, carousel3, carousel4, carousel5];
+
+const Carousel = ({}) => {
+  const settings = {
+    dots: true,
+    lazyLoad: true,
+    fade: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    slidesToScroll: 1,
+    className: 'slick-slider-fade',
+    appendDots: (dots) => <ul> {dots} </ul>,
+    customPaging: () => (
+      <div className="list-inner">
+        <div className="button" />
+      </div>
+    ),
+  };
+
+  return (
+    <main id="carousel" className={styles.main}>
+      <Slider {...settings}>
+        {images.map((image) => (
+          <div key={image}>
+            <div
+              className={styles.image}
+              style={{
+                backgroundImage: `url(${image})`,
+              }}
+            />
+          </div>
+        ))}
+      </Slider>
+    </main>
+  );
+};
+
+export default Carousel;
--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 // components
 import TopBanner from '../../component/TopBanner';
+import Carousel from '../../component/Carousel';
 import ContentList from '../../component/ContentList';
 
 // data
@@ -13,6 +14,7 @@ const Home = () => {
   return (
     <div className={styles.body}>
       <TopBanner />
+      <Carousel />
       <div>
         {contentLists.map((contentList) => (
           <ContentList key={contentList.id} contentList={contentList} />
